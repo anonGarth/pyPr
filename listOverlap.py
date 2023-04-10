ea1331fbb93986a6c055fbfa137441f5f503001b
annonGarth <greggarthshaw@gmail.com>
2023-04-09 18:03:52 -0600

    adjusted ranges

--- listOverlap.py
+++ listOverlap.py
@@ -11,7 +11,7 @@ WITHOUT DUPLICATES IN THE NEW LIST AND USE LIST COMPREHENSION
 """
 
 import random
-listA = [random.randrange(1,20) for x in range(10)] #fill list a with 20 numbers 1 through 19
-listB = [random.randrange(1,20) for x in range(10)]
+listA = [random.randrange(0,20) for x in range(10)] #fill list a with 20 numbers 1 through 19
+listB = [random.randrange(0,20) for x in range(10)]
 finalList = [x for x in set(listA) if x in listB]
 print(finalList)
